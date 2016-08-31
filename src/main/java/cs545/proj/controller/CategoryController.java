3e86e1457a1e655e608019af78aa62ccca7b4807
Gebremedhn <gebremedhnm.gebru@gmail.com>
2016-08-31 17:22:30 -0500

    New category list

--- src/main/java/cs545/proj/controller/CategoryController.java
+++ src/main/java/cs545/proj/controller/CategoryController.java
@@ -1,16 +1,22 @@
 package cs545.proj.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import cs545.proj.domain.Category;
+import cs545.proj.domain.Member;
 import cs545.proj.service.CategoryService;
 
 
@@ -27,5 +33,24 @@ public class CategoryController {
          categoryService.saveOrUpdate(category);
          return category;
     }
+    
+
+	@RequestMapping("/category_edit/{id}")
+	@PreAuthorize("hasRole('ROLE_ADMIN')") 
+	public String getEditCatalogForm(@PathVariable("id") int id, Model model) {
+		Category category = categoryService.getCategoryById(id);
+		model.addAttribute("catagory", category );
+		   return "editCategory";
+		}
+	
+	@RequestMapping(value="/member_update" ,method = RequestMethod.POST)
+//	@PreAuthorize("hasRole('ROLE_ADMIN')") 
+	public String categoryupdate(@ModelAttribute("newMember") @Valid Category categoryToBeEdited, BindingResult result, HttpServletRequest request) {
+		if(result.hasErrors()) {
+			return "editCategory";
+		}
+		categoryService.saveOrUpdate(categoryToBeEdited);
+	   	return "redirect:/categoryList";
+	}
 
 }
---
+++ src/main/webapp/WEB-INF/views/editCategory.jsp
@@ -0,0 +1,39 @@
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Edit Category</title>
+<style type="text/css">@import url("<c:url value="/css/main.css"/>");</style>
+</head>
+<body>
+
+<spring:url var = "category_update" value="/category_update" />
+
+
+<div id="global">
+<form:form commandName="catagory" action= "${category_update}" >
+    <fieldset>
+        <legend>Edit a category</legend>
+        <form:hidden path="id"/>
+        
+        <p>
+            <label for="name">Name : </label>
+            <form:input id="name" path="name"/>
+        </p>
+        <p>
+            <label for="description">Author: </label>
+            <form:input id="description" path="description"/>
+        </p>
+        
+        <p id="buttons">
+            <input id="reset" type="reset" tabindex="4">
+            <input id="submit" type="submit" tabindex="5" 
+                value="Update Book">
+        </p>
+    </fieldset>
+</form:form>
+</div>
+</body>
+</html>
